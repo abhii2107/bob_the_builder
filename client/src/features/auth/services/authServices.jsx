f2a30dc3af5cbefc15b67317b711b263a47cfe77
abhii2107 <bhatiaabhishek776@gmail.com>
2026-09-04 10:52:25 +0530

    feat: refactor loginUser to use centralized API instance

--- client/src/features/auth/services/authServices.jsx
+++ client/src/features/auth/services/authServices.jsx
@@ -1,10 +1,8 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:5000/api/v1";
+import api from "@/api/axios";
 
 export const loginUser = async (credentials) => {
-  const response = await axios.post(
-    `${API_URL}/auth/login`,
+  const response = await api.post(
+    "/auth/login",
     credentials
   );
 
